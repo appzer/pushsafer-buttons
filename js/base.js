7cbf6e7b3c458fa87a445daf384a7f0181964ccc
Kevin Siml <appzer@users.noreply.github.com>
2022-05-25 16:59:48 +0200

    Add files via upload

--- js/base.js
+++ js/base.js
@@ -1,1 +1,122 @@
+$( document ).ready(function() {
+    console.log( "ready!");
+	
+	// Change Background Image
+	$('#bg').css("background-image", "url('images/bg/"+background_image+".jpg')");
+	var logo='', cols = 3; innerHTML='', ButtonColor='#000', ButtonBorderColor='#000', ButtonBorderRadius='10', ButtonTextColor='#FFF', ButtonTextSize='2', PS_Title='', PS_Message='Empty Message', PS_Device='a', PS_Sound='1', PS_Vibration='0', PS_Icon='1', PS_IconColor='', PS_Priority='0', PS_Url='', PS_UrlTitle='', PS_Answer='0', PS_AnswerOption='', PS_Confirm='0';
+	
+	if(buttons_in_row==12){ cols = 1; }
+	if(buttons_in_row==6){ cols = 2; }
+	if(buttons_in_row==4){ cols = 3; }
+	if(buttons_in_row==3){ cols = 4; }
+	if(buttons_in_row==2){ cols = 6; }
+	if(buttons_in_row==1){ cols = 12; }
+	
+	Buttons.forEach(function (m, i) {
+		for (preferences in m) {
+			if(preferences=='ButtonColor'){
+				ButtonColor = m[preferences];
+			}
+			if(preferences=='ButtonBorderColor'){
+				ButtonBorderColor = m[preferences];
+			}
+			if(preferences=='ButtonBorderRadius'){
+				ButtonBorderRadius = m[preferences];
+			}
+			if(preferences=='ButtonTextColor'){
+				ButtonTextColor = m[preferences];
+			}
+			if(preferences=='ButtonTextSize'){
+				ButtonTextSize = m[preferences];
+			}
+			if(preferences=='PS_Title'){
+				PS_Title = m[preferences];
+			}
+			if(preferences=='PS_Message'){
+				PS_Message = m[preferences];
+			}
+			if(preferences=='PS_Device'){
+				PS_Device = m[preferences];
+			}
+			if(preferences=='PS_Sound'){
+				PS_Sound = m[preferences];
+			}
+			if(preferences=='PS_Vibration'){
+				PS_Vibration = m[preferences];
+			}
+			if(preferences=='PS_Icon'){
+				PS_Icon = m[preferences];
+			}
+			if(preferences=='PS_IconColor'){
+				PS_IconColor = m[preferences];
+			}
+			if(preferences=='PS_Priority'){
+				PS_Priority = m[preferences];
+			}
+			if(preferences=='PS_Url'){
+				PS_Url = m[preferences];
+			}
+			if(preferences=='PS_UrlTitle'){
+				PS_UrlTitle = m[preferences];
+			}
+			if(preferences=='PS_Answer'){
+				PS_Answer = m[preferences];
+			}
+			if(preferences=='PS_AnswerOption'){
+				PS_AnswerOption = m[preferences];
+			}
+			if(preferences=='PS_Confirm'){
+				PS_Confirm = m[preferences];
+			}
+			if(preferences=='ButtomName'){
+				innerHTML += "<div onclick=\"send('"+PS_Title+"','"+PS_Message+"','"+PS_Device+"','"+PS_Sound+"','"+PS_Vibration+"','"+PS_Icon+"','"+PS_IconColor+"','"+PS_Priority+"','"+PS_Url+"','"+PS_UrlTitle+"','"+PS_Answer+"','"+PS_AnswerOption+"','"+PS_Confirm+"')\" class=\"btnholder col-"+cols+"\"><button style=\"font-size:"+ButtonTextSize+"rem;color:"+ButtonTextColor+";background-color:"+ButtonColor+"\; border-radius: "+ButtonBorderRadius+"px; border: 1px solid "+ButtonBorderColor+"\" class=\"btn btn-primary btn-block\">" + m[preferences] + "</button></div>";
+			}
+		}
+	});
+	logo = "<div id=\"logo\"><img src=\"images/logo.png\"></div>";
+	$('#bg').html (logo+innerHTML);
+	$('#logo').css("background-color", logo_background_color);
+	$('.btn').css("height", buttons_height);
+});
 
+function send(title,message,device,sound,vibration,icon,iconcolor,priority,Url,UrlTitle,Answer,AnswerOption,Confirm){
+	$.ajax({
+	  type: "POST",
+	  url: 'https://www.pushsafer.com/api',
+	  data:  {
+			t: escape(title),
+			m: escape(message),
+			s: sound,
+			v: vibration,
+			i: icon,
+			c: iconcolor,
+			d: device,
+			pr: priority,
+			a: Answer,
+			ao: AnswerOption,
+			cr: Confirm,
+			u: escape(Url),
+			ut: escape(UrlTitle),
+			k: private_key
+	  }
+	}).done(function(data) {
+		var obj = jQuery.parseJSON(data);
+		if(show_response_json==false) {data='';}
+		if(obj.status==1){
+			Swal.fire({
+			  title: Success,
+			  text: data,
+			  icon: 'success',
+			  confirmButtonText: OK
+			})
+		} else {
+			Swal.fire({
+			  title: Error,
+			  text: data,
+			  icon: 'error',
+			  confirmButtonText: OK
+			})
+		}
+	});
+	
+}
---
+++ js/config.js
@@ -0,0 +1,149 @@
+// Pushsafer Things
+var private_key = '{privatekey}';
+var show_response_json = true;
+
+// Translations
+var Success = "Success";
+var Error = "Error";
+var OK = "OK";
+
+// Style
+var background_image = 1; // 1-10
+var logo_background_color = 'rgba(255,255,255,0.75)';
+var buttons_in_row = 2; // 1,2,3,4,6,12
+var buttons_height = 100;
+
+// Buttons
+var Buttons = new Array();
+
+// Button 1
+Buttons[0] = new Object();
+Buttons[0]["PS_Title"] = "Title 1";
+Buttons[0]["PS_Message"] = "Message 1";
+Buttons[0]["PS_Device"] = "{deviceid}";
+Buttons[0]["PS_Sound"] = "1";
+Buttons[0]["PS_Vibration"] = "0";
+Buttons[0]["PS_Icon"] = "11";
+Buttons[0]["PS_IconColor"] = "#FF0000";
+Buttons[0]["PS_Priority"] = "0";
+Buttons[0]["PS_Url"] = "https://www.pushsafer.com";
+Buttons[0]["PS_UrlTitle"] = "Open Pushsafer";
+Buttons[0]["PS_Answer"] = "1";
+Buttons[0]["PS_AnswerOption"] = "Yes|No|Maybe";
+Buttons[0]["PS_Confirm"] = "10";
+Buttons[0]["ButtonColor"] = "rgba(33,150,243,0.5)";
+Buttons[0]["ButtonBorderColor"] = "blue";
+Buttons[0]["ButtonBorderRadius"] = "50";
+Buttons[0]["ButtonTextColor"] = "white";
+Buttons[0]["ButtonTextSize"] = "2";
+Buttons[0]["ButtomName"] = "Button 1";
+
+// Button 2
+Buttons[1] = new Object();
+Buttons[1]["PS_Title"] = "Title 2";
+Buttons[1]["PS_Message"] = "Message 2";
+Buttons[1]["PS_Device"] = "{deviceid}";
+Buttons[1]["PS_Sound"] = "10";
+Buttons[1]["PS_Vibration"] = "0";
+Buttons[1]["PS_Icon"] = "11";
+Buttons[1]["PS_IconColor"] = "#F0FF0F";	
+Buttons[1]["PS_Priority"] = "1";
+Buttons[1]["PS_Url"] = "https://www.pushsafer.com";
+Buttons[1]["PS_UrlTitle"] = "Open Pushsafer";
+Buttons[1]["PS_Answer"] = "1";
+Buttons[1]["PS_AnswerOption"] = "Yes|No|Maybe";
+Buttons[1]["PS_Confirm"] = "0";
+Buttons[1]["ButtonColor"] = "rgba(255,0,0,0.7)";
+Buttons[1]["ButtonBorderColor"] = "black";
+Buttons[1]["ButtonBorderRadius"] = "50";
+Buttons[1]["ButtonTextColor"] = "white";
+Buttons[1]["ButtonTextSize"] = "2";
+Buttons[1]["ButtomName"] = "Button 2";
+
+// Button 3
+Buttons[2] = new Object();
+Buttons[2]["PS_Title"] = "Title 3";
+Buttons[2]["PS_Message"] = "Message 3";
+Buttons[2]["PS_Device"] = "{deviceid}";
+Buttons[2]["PS_Sound"] = "40";
+Buttons[2]["PS_Vibration"] = "2";
+Buttons[2]["PS_Icon"] = "11";
+Buttons[2]["PS_IconColor"] = "";
+Buttons[2]["PS_Priority"] = "0";
+Buttons[2]["PS_Url"] = "https://www.pushsafer.com";
+Buttons[2]["PS_UrlTitle"] = "Open Pushsafer";
+Buttons[2]["PS_Answer"] = "1";
+Buttons[2]["PS_AnswerOption"] = "Yes|No|Maybe";
+Buttons[2]["PS_Confirm"] = "0";
+Buttons[2]["ButtonColor"] = "rgba(255,255,255,0.75)";
+Buttons[2]["ButtonBorderColor"] = "black";
+Buttons[2]["ButtonBorderRadius"] = "50";
+Buttons[2]["ButtonTextColor"] = "black";
+Buttons[2]["ButtonTextSize"] = "2";
+Buttons[2]["ButtomName"] = "Button 3";
+
+// Button 4
+Buttons[3] = new Object();
+Buttons[3]["PS_Title"] = "Title 4";
+Buttons[3]["PS_Message"] = "Message 4";
+Buttons[3]["PS_Device"] = "{deviceid}";
+Buttons[3]["PS_Sound"] = "21";
+Buttons[3]["PS_Vibration"] = "0";
+Buttons[3]["PS_Icon"] = "11";
+Buttons[3]["PS_IconColor"] = "#00FF00";
+Buttons[3]["PS_Priority"] = "1";
+Buttons[3]["PS_Url"] = "https://www.pushsafer.com";
+Buttons[3]["PS_UrlTitle"] = "Open Pushsafer";
+Buttons[3]["PS_Answer"] = "1";
+Buttons[3]["PS_AnswerOption"] = "Yes|No|Maybe";
+Buttons[3]["PS_Confirm"] = "0";
+Buttons[3]["ButtonColor"] = "rgba(255,255,0,0.75)";
+Buttons[3]["ButtonBorderColor"] = "black";
+Buttons[3]["ButtonBorderRadius"] = "50";
+Buttons[3]["ButtonTextColor"] = "black";
+Buttons[3]["ButtonTextSize"] = "2";
+Buttons[3]["ButtomName"] = "Button 4";
+
+// Button 5
+Buttons[4] = new Object();
+Buttons[4]["PS_Title"] = "Title 5";
+Buttons[4]["PS_Message"] = "Message 5";
+Buttons[4]["PS_Device"] = "{deviceid}";
+Buttons[4]["PS_Sound"] = "14";
+Buttons[4]["PS_Vibration"] = "2";
+Buttons[4]["PS_Icon"] = "41";
+Buttons[4]["PS_IconColor"] = "#FF0000";
+Buttons[4]["PS_Priority"] = "2";
+Buttons[4]["PS_Url"] = "https://www.pushsafer.com";
+Buttons[4]["PS_UrlTitle"] = "Open Pushsafer";
+Buttons[4]["PS_Answer"] = "1";
+Buttons[4]["PS_AnswerOption"] = "Yes|No|Maybe";
+Buttons[4]["PS_Confirm"] = "0";
+Buttons[4]["ButtonColor"] = "rgba(100,100,100,0.75)";
+Buttons[4]["ButtonBorderColor"] = "black";
+Buttons[4]["ButtonBorderRadius"] = "50";
+Buttons[4]["ButtonTextColor"] = "black";
+Buttons[4]["ButtonTextSize"] = "2";
+Buttons[4]["ButtomName"] = "Button 5";
+
+// Button 6
+Buttons[5] = new Object();
+Buttons[5]["PS_Title"] = "Title 6";
+Buttons[5]["PS_Message"] = "Message 6";
+Buttons[5]["PS_Device"] = "{deviceid}";
+Buttons[5]["PS_Sound"] = "1";
+Buttons[5]["PS_Vibration"] = "0";
+Buttons[5]["PS_Icon"] = "11";
+Buttons[5]["PS_IconColor"] = "#FF0000";
+Buttons[5]["PS_Priority"] = "0";
+Buttons[5]["PS_Url"] = "https://www.pushsafer.com";
+Buttons[5]["PS_UrlTitle"] = "Open Pushsafer";
+Buttons[5]["PS_Answer"] = "1";
+Buttons[5]["PS_AnswerOption"] = "Yes|No|Maybe";
+Buttons[5]["PS_Confirm"] = "10";
+Buttons[5]["ButtonColor"] = "rgba(33,150,243,0.5)";
+Buttons[5]["ButtonBorderColor"] = "blue";
+Buttons[5]["ButtonBorderRadius"] = "50";
+Buttons[5]["ButtonTextColor"] = "white";
+Buttons[5]["ButtonTextSize"] = "2";
+Buttons[5]["ButtomName"] = "Button 6";
